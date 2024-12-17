e1ac6d2942a7fc2799cd1767d4e4ae084d5edf86
Peter Weber <peter.weber@rero.ch>
2024-12-17 16:56:58 +0100

    chore: update dependencies
    Co-Authored-by: Peter Weber <peter.weber@rero.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -35,12 +35,38 @@ trap cleanup EXIT
 # python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 
-safety_exceptions="-i 51668 -i 42194 -i 62019 -i 67599 -i 51457"
+# -> Vulnerability found in py version 1.11.0
+#    Vulnerability ID: 51457
+#    For more information about this vulnerability, visit
+#    To ignore this vulnerability, use PyUp vulnerability id 51457 in safety’s
+# -> Vulnerability found in pip version 22.3.1
+#    Vulnerability ID: 62044
+#    For more information about this vulnerability, visit
+#    To ignore this vulnerability, use PyUp vulnerability id 62044 in safety’s
+# -> Vulnerability found in sqlalchemy-utils version 0.41.2
+#    Vulnerability ID: 42194
+#    For more information about this vulnerability, visit
+#    To ignore this vulnerability, use PyUp vulnerability id 42194 in safety’s
+# -> Vulnerability found in flask-cors version 5.0.0
+#    Vulnerability ID: 70624
+#    For more information about this vulnerability, visit
+#    To ignore this vulnerability, use PyUp vulnerability id 70624 in safety’s
+# -> Vulnerability found in flask-cors version 5.0.0
+#    Vulnerability ID: 72731
+#    ADVISORY: A vulnerability in corydolphin/flask-cors allows the
+#    For more information about this vulnerability, visit
+#    To ignore this vulnerability, use PyUp vulnerability id 72731 in safety’s
+# -> Vulnerability found in flask-caching version 2.3.0
+#    Vulnerability ID: 40459
+#    For more information about this vulnerability, visit
+#    To ignore this vulnerability, use PyUp vulnerability id 40459 in safety’s
+safety_exceptions="-i 51457 -i 62044 -i 42194 -i 70624 -i 72731 -i 40459"
 msg=$(safety check -o text ${safety_exceptions}) || {
     echo "Safety vulnerabilites found for packages:" $(safety check -o bare ${safety_exceptions})
     echo "Run: \"safety check -o screen ${safety_exceptions} | grep -i vulnerability\" for more details"
     exit 1
   }
+
 
 autoflake -r --remove-all-unused-imports --ignore-init-module-imports --quiet .
 
